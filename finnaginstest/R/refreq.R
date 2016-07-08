4f286525872f053a856484479b023601c47639ac
finnagin <flan313@gmail.com>
2016-07-08 10:25:49 -0700

    Add files via upload

--- finnaginstest/R/refreq.R
+++ finnaginstest/R/refreq.R
@@ -1,21 +1,21 @@
 #' Request from refence server
-#' @description Sends a request to the framespace reference server and retreaves the content of the response
+#' @description Sends a request in the form of a json file to the framespace reference server and retreaves the content of the response
 #' @param url = the url you want the data to come from i.e. "http://example.com/api/"
-#' @param end = the prefex to the above url i.e. "example/"
-#' @param req = the request you want to send
+#' @param end = the prefex to the above url i.e. "example/search/"
+#' @param file = a json object containing the search request
 #' @return a list containig the information
 #' @export
-refreq <- function(url = "http://0.0.0.0:5000/", end = "units/search", req = {}){
+refreq <- function(url = "http://0.0.0.0:5000/", end = "units/search", file = "searchall.json"){
 
 
   url <- paste0(url, end)
-  r <- POST(url,
+  req <- POST(url,
             encode = "json",
             accept_json(),
             add_headers(`Accept` = 'application/json',
                         `Content-Type` = 'application/json'),
-            body = toJSON(req))
+            body = upload_file(file))
 
-  response <- content(r, as = "parsed")
+  response <- content(req, as = "parsed")
   return(response)
 }
